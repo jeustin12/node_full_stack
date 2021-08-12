a657fc9192f2988abbe107539e1c37ae313249fc
justin <jeustinespinoza12@gmail.com>
2021-08-12 09:03:50 -0600

    a

--- index.js
+++ index.js
@@ -4,96 +4,99 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 let persons = [
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 1,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 2,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 3,
-  },
-  {
-    name: "lol-pallo",
-    number: "1",
-    id: 4,
-  },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 1,
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 2,
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 3,
+    },
+    {
+        name: "lol-pallo",
+        number: "1",
+        id: 4,
+    },
 ];
 morgan.token("body", function (req) {
-  return JSON.stringify(req.body);
+    return JSON.stringify(req.body);
 });
 app.use(express.json());
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :body"
+    )
 );
 app.use(cors());
 app.get("/info", (request, response) => {
-  let info = `
+    let info = `
     Phonebook has info for ${persons.length} people
     <br/><br/>
     ${new Date()}
   `;
-  response.send(info);
+    response.send(info);
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+    response.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  console.log(person);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+    console.log(person);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
+    const maxId =
+        persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+    return maxId + 1;
 };
 app.post("/api/persons", (request, response) => {
-  const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-  const exist = persons.find(
-    (element) => element.name.toLowerCase() === body.name.toLowerCase()
-  );
-  if (exist) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-  const person = {
-    name: body.name,
-    number: body.number || false,
-    id: generateId(),
-  };
+    const body = request.body;
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: "content missing",
+        });
+    }
+    const exist = persons.find(
+        (element) => element.name.toLowerCase() === body.name.toLowerCase()
+    );
+    if (exist) {
+        return response.status(400).json({
+            error: "name must be unique",
+        });
+    }
+    const person = {
+        name: body.name,
+        number: body.number || false,
+        id: generateId(),
+    };
 
-  persons = persons.concat(person);
+    persons = persons.concat(person);
 
-  response.json(person);
+    response.json(person);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+    const id = Number(request.params.id);
+    persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
+    response.status(204).end();
 });
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
